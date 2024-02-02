7b4157bbe5f47a33fee02ecd9bc5409fd4317c98
Yookyubin <binde73@naver.com>
2024-02-02 18:38:43 +0900

    간단한 장애물 회피 시뮬레이션

--- Assets/ObstacleAvoidanceTest.cs
+++ Assets/ObstacleAvoidanceTest.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +10,7 @@ public class ObstacleAvoidanceTest : MonoBehaviour
     public float distance;
     private const int numPoints = 30;
     public float angle;
+    public float speed;
     [SerializeField]
     LayerMask obstacleLayer;
     Transform detectPos;
@@ -16,11 +19,12 @@ public class ObstacleAvoidanceTest : MonoBehaviour
     void Start()
     {
         dirs = new Vector3[numPoints];
-        distance = 2.0f;
+        distance = 7.0f;
         //numPoints = 30;
-        angle = Mathf.PI * 0.5f;
+        angle = Mathf.PI * 0.75f;
+
         obstacleLayer = LayerMask.GetMask("Obstacle");
-        this.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        this.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new UnityEngine.Color(Random.value, Random.value, Random.value);
         detectPos = this.transform.GetChild(0).transform;
 
         for (int i=0; i<numPoints; i++)
@@ -36,25 +40,49 @@ public class ObstacleAvoidanceTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 moveDir = this.transform.forward;
+        RaycastHit hit;
+        float maxDist = 0f;
         for (int i=0; i<numPoints; i++)
         {
-            //float theta = angle * i / (numPoints - 1f);
-            //float z = Mathf.Cos(theta);
-            //float x = Mathf.Sin(theta) * Mathf.Pow(-1, i % 2);
-            //Vector3 dir = new Vector3(x, 0, z);
-            //dirs[i] = dir;
-            // -------------- 이 위의 연산들은 start 에 한번만으로도 가능해보임 ----------
-            Color color = Color.green;
+            UnityEngine.Color color = UnityEngine.Color.green;
             Vector3 dir = transform.localRotation * dirs[i]; // dir을 로컬 좌표계로 변환하기 위함
-            //dirs[i] = dir;
+            //Vector3 dir = transform.TransformDirection(dirs[i]);
 
-            if (Physics.Raycast(detectPos.position, dir, this.distance, obstacleLayer))
+            if (Physics.SphereCast(detectPos.position, 0.5f, dir, out hit, this.distance, obstacleLayer))
             {
-                color = Color.red;
+                color = UnityEngine.Color.red;
+                if (hit.distance > maxDist)
+                {
+                    maxDist = hit.distance;
+                    moveDir = dir;
+                }
             }
+            else
+            {
+                moveDir = dir;
+                break;
+            }
+
 
             Debug.DrawLine(detectPos.position, detectPos.position + dir * distance, color);
         }
-        Debug.Log(dirs[2]); // 매 update 마다 localRoation 적용 이전의 dir 을 구할 필요가 없음
+
+        Debug.DrawLine(detectPos.position, detectPos.position + moveDir * distance, UnityEngine.Color.green);
+        //Debug.Log(dirs[2]); // 매 update 마다 localRoation 적용 이전의 dir 을 구할 필요가 없음
+
+        moveDir = Vector3.Lerp(this.transform.forward, moveDir, Time.deltaTime * 5);
+        moveDir.Normalize();
+
+        MoveFoward(moveDir);
+        
+        // 속도, 광선 길이, sphere cast의 반지름, lerp 속도 등으로 다양하게 회전 구현 가능
+    }
+
+
+    void MoveFoward(Vector3 direction)
+    {
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.position += transform.forward * Time.deltaTime * speed;
     }
 }
